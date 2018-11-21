66be6b1f874651d865207baff9af7d06abe2e323
Metacom-Carlos <44421555+Metacom-Carlos@users.noreply.github.com>
2018-11-20 19:23:21 -0800

    night pt.2

---
+++ Assets/Healthpack.cs
@@ -0,0 +1,33 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthpack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    function OnTriggerEnter2D ()
+    {
+        if (life >= 80)
+        {
+            life = 100;
+        }
+        else
+        {
+            Script.life += 20;
+        }
+        print("Gained 20 health");
+        Destroy(gameObject);
+
+    }
+
+    
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
+*/
