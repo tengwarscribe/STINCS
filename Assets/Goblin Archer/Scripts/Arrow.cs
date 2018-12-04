9ca38571326d03c9fb703f49dd9a0254fa79f0ce
tengwarscribe <tengwarwriter@yahoo.com>
2018-12-04 15:37:11 -0800

    Update Arrow.cs

--- Assets/Goblin Archer/Scripts/Arrow.cs
+++ Assets/Goblin Archer/Scripts/Arrow.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Arrow : MonoBehaviour {
@@ -6,8 +6,9 @@ public class Arrow : MonoBehaviour {
 	public Vector3 v = new Vector3(5, 3, 0);
 	public Vector3 a = new Vector3(0, -10, 0);
     public bool right = true;
+    public int damage;
 
-	void Start () {
+    void Start () {
 		Destroy(this.gameObject, 10);
         if (!right)
             v.x = -v.x;
@@ -18,6 +19,28 @@ public class Arrow : MonoBehaviour {
 		transform.position += v*Time.deltaTime;
 		v += a * Time.deltaTime;
         
-        transform.rotation = Quaternion.LookRotation(v, new Vector3(0,0,-1)); 
-	}
+        transform.rotation = Quaternion.LookRotation(v, new Vector3(0,0,-1));
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Health otherProjectile = collision.gameObject.GetComponent<Health>();
+
+            Debug.Log("I am colliding with another object!");
+
+            if (otherProjectile.hitPoints <= 80)
+            {
+                otherProjectile.hitPoints -= 20;
+            }
+
+            else if (otherProjectile.hitPoints > 80)
+            {
+                otherProjectile.hitPoints = 100;
+            }
+
+            Destroy(gameObject);
+        }
+    }
 }
