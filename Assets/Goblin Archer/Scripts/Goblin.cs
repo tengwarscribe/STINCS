14390d1e7caf3867bb2b830cc6b0f5c14ee48255
Metacom-Carlos <44421555+Metacom-Carlos@users.noreply.github.com>
2018-12-04 15:30:44 -0800

    goblin

--- Assets/Goblin Archer/Scripts/Goblin.cs
+++ Assets/Goblin Archer/Scripts/Goblin.cs
@@ -30,7 +30,7 @@ public class Goblin : MonoBehaviour
 
     void Update()
     {
-        if (time < 150)
+        /*if (time < 150)
         {
             time = time + 1;
         }
@@ -39,7 +39,7 @@ public class Goblin : MonoBehaviour
             if (Random.Range(0f, 0.25f) > 0.125f)
                 animator.SetTrigger("attack");
             StartCoroutine(makeArrow(arrowDelay, lookRight));
-        }
+        }*/
 
        /* RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.localScale.x * Vector3.right, distance);
         if (hit.collider.tag == "Player")
